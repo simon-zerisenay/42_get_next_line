599ca4e9858ca7a29c6c7717222d5154c4a6ee6f
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-02-11 14:48:24 +0400

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -12,6 +12,9 @@
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <limits.h>
 # include <stdlib.h>
 # include <sys/types.h>
