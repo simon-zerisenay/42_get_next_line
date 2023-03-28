7d15b5446b6f067eb4a7136720f3b4b287efe527
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-03-28 19:15:24 +0400

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -22,7 +22,7 @@
 # include <unistd.h>  
 
 char	*get_next_line(int fd); 
-char	*ft_strjoin(char const *s1, char const *s2);  
+char	*ft_strjoin(char const *s1, char const *s2);   
 char	*ft_strchr(const char *string, int searchedChar);    
     
 void	ft_bzero(void *s, size_t n);  
