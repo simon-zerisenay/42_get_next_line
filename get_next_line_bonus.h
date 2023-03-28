bacf5ccb2590300637d564e9f32e4130d686dc02
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-03-28 19:15:30 +0400

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -26,7 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *string, int searchedChar);    
     
 void	ft_bzero(void *s, size_t n);  
-void	*ft_calloc(size_t elementCount, size_t elementSize);
+void	*ft_calloc(size_t elementCount, size_t elementSize); 
 
 size_t	ft_strlen(const char *theString);
  
